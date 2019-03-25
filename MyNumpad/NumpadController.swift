27849d07b9fb533f5aeea6d018edbce5c7159d9f
jinuman <jin35kim@gmail.com>
2019-03-26 02:16:27 +0900

    feat: Design item cells

--- MyNumpad/NumpadController.swift
+++ MyNumpad/NumpadController.swift
@@ -8,16 +8,72 @@
 
 import UIKit
 
+class NumpadCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor(white: 0.9, alpha: 1)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 class NumpadController: UICollectionViewController {
     
-    
+    let cellId = "cellId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        collectionView.backgroundColor = .purple
+        collectionView.backgroundColor = .white
+        collectionView.register(NumpadCell.self, forCellWithReuseIdentifier: cellId)
     }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        collectionView.collectionViewLayout.invalidateLayout()
+    }
+    
+}
 
-
+// MARK:- Regarding collection view
+extension NumpadController: UICollectionViewDelegateFlowLayout {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as? NumpadCell else {
+            fatalError("NumpadCell is bad")
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let leftRightsectionPadding = view.frame.width * 0.13
+        let interSpacing = view.frame.width * 0.1
+        
+        let cellWidth = (view.frame.width - 2 * leftRightsectionPadding - 2 * interSpacing) / 3
+        
+        return CGSize(width: cellWidth, height: cellWidth)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        let leftRightsectionPadding = view.frame.width * 0.15
+        let interSpacing = view.frame.width * 0.1
+        
+        
+        return UIEdgeInsets(top: 16,
+                            left: leftRightsectionPadding,
+                            bottom: 16,
+                            right: leftRightsectionPadding)
+    }
 }
 
