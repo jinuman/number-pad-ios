f9ef8b300b9469d277c0253b3cbbc78bf17f14c7
jinuman <jin35kim@gmail.com>
2019-03-25 22:46:59 +0900

    feat: Initialize project

---
+++ MyNumpad/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MyNumpad
+//
+//  Created by Jinwoo Kim on 25/03/2019.
+//  Copyright © 2019 jinuman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .purple
+    }
+
+
+}
+
